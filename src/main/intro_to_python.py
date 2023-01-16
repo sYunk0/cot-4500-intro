a169429e35fe51a733b9602ba4427d91d936f824
Sam <801sly@gmail.com>
2023-01-16 16:14:26 -0500

    Created base file setup

---
+++ src/main/intro_to_python.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    matrix1 = np.identity(3).astype("int32")
+    matrix2 = np.where(matrix1 == 1,matrix1,3)
+    matrix3 = matrix2[:,:2]
+
+    print(matrix1)
+    print()
+    print(matrix2)
+    print()
+    print(matrix3)
